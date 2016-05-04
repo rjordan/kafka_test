ce650a12282b8ff6064a942f76485ea8cd82cd38
Richard Kent Jordan <rjordan@pobox.com>
2016-05-03 23:54:03 -0500

    Code more Kotlin like.

--- src/main/kotlin/org/codecrusade/main.kt
+++ src/main/kotlin/org/codecrusade/main.kt
@@ -1,58 +1,42 @@
 package org.codecrusade
 
-import org.apache.kafka.clients.KafkaClient
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.TopicPartition
-import org.apache.kafka.common.errors.WakeupException
-import org.apache.kafka.common.network.KafkaChannel
-import org.apache.kafka.common.security.auth.KafkaPrincipal
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.log4j.BasicConfigurator
-import java.util.*
-import java.util.logging.Logger
-
-var logger = Logger.getGlobal()
 
 fun main(args: Array<String>) {
     BasicConfigurator.configure();
 
-    val topic = "my_strange_topic"
+    val topic = "test"
 
-    val props = Properties()
-    props.put("bootstrap.servers", "192.168.0.60:9092")
-    props.put("group.id", "consumer-tutorial")
-    props.put("key.deserializer", StringDeserializer::class.java.name )
-    props.put("value.deserializer", StringDeserializer::class.java.name)
-    //props.put("enable.auto.commit","false")
-    props.put("auto.offset.reset","earliest")
-    //props.put("metrics.num.samples","1")
-    val consumer = KafkaConsumer<String,Any>(props)
-    consumer.subscribe(listOf(topic))
-    val mets = consumer.metrics()
+    val cThread = Thread(Runnable {
+        consumerThread(topic)
+    })
+    cThread.start()
 
     val pThread = Thread(Runnable {
-        val props = Properties()
-        props.put("bootstrap.servers", "192.168.0.60:9092")
-        //props.put("zookeeper.connect", "192.168.0.60:2181")
-        props.put("key.serializer", StringSerializer::class.java.name )
-        props.put("value.serializer", StringSerializer::class.java.name)
-        props.put("acks", "1")
-        val producer = KafkaProducer<String,Any>(props)
-        try {
-            while(true) {
-                producer.send(ProducerRecord(topic, "key", "{}"));
-                Thread.sleep(200)
-            }
-        }
-        finally {
-            producer.close()
-        }
+        producerThread(topic)
     })
     pThread.start()
 
+    while (true) {
+    }
+    //    pThread.join()
+    //    cThread.join()
+}
+
+private fun consumerThread(topic: String) {
+    val consumer = KafkaConsumer<String, Any>(mapOf(
+            "bootstrap.servers" to "192.168.0.60:9092",
+            "group.id" to "consumer-tutorial",
+            "key.deserializer" to StringDeserializer::class.java.name,
+            "value.deserializer" to StringDeserializer::class.java.name,
+            "auto.offset.reset" to "earliest"
+    ))
+    consumer.subscribe(listOf(topic))
     try {
         while (true) {
             val records = consumer.poll(Long.MAX_VALUE);
@@ -60,10 +44,24 @@ fun main(args: Array<String>) {
                 println("#{it.offset()} : #{it.value()}")
             }
         }
-    } catch (e: WakeupException) {
-        // ignore for shutdown
     } finally {
-        pThread.join()
         consumer.close()
+    }
+}
+
+private fun producerThread(topic: String) {
+    val producer = KafkaProducer<String, Any>(mapOf(
+            "bootstrap.servers" to "192.168.0.60:9092",
+            "key.serializer" to StringSerializer::class.java.name,
+            "value.serializer" to StringSerializer::class.java.name,
+            "acks" to "1"
+    ))
+    try {
+        while (true) {
+            producer.send(ProducerRecord(topic, "key", "{\"FOO\":\"BAR\"}"));
+            Thread.sleep(200)
+        }
+    } finally {
+        producer.close()
     }
 }
